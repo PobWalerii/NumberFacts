5b5bacc24a1b22bd8bfc8339238ac8bab6fa7cb8
PobWalerii <lstshef@gmail.com>
2023-02-24 17:20:06 +0200

    Делаем repository

--- app/src/main/java/com/example/numberfacts/data/repository/NumbersRepository.kt
+++ app/src/main/java/com/example/numberfacts/data/repository/NumbersRepository.kt
@@ -2,6 +2,10 @@ package com.example.numberfacts.data.repository
 
 import com.example.numberfacts.data.api.ApiService
 import com.example.numberfacts.data.database.dao.NumbersDao
+import com.example.numberfacts.data.database.entitys.Numbers
+import com.example.numberfacts.utils.ResponseState
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 class NumbersRepository @Inject constructor(
@@ -9,11 +13,17 @@ class NumbersRepository @Inject constructor(
     private val numbersDao: NumbersDao
 ){
 
-    fun getNumbersFacts(key: Int) {
+    fun getNumbersFacts(key: Int): Flow<ResponseState<List<Numbers>>> {
+        return flow {
 
 
 
 
+
+
+
+
+        }
     }
 
 
--- app/src/main/java/com/example/numberfacts/utils/NumberUiState.kt
+++ app/src/main/java/com/example/numberfacts/utils/NumberUiState.kt
@@ -3,7 +3,7 @@ package com.example.numberfacts.utils
 import com.example.numberfacts.data.database.entitys.Numbers
 
 sealed class NumbersUiState
-    data class FactsLoaded(val data: Numbers) : NumbersUiState()
+    data class FactsLoaded(val data: List<Numbers>) : NumbersUiState()
     data class ListLoading(val isLoading: Boolean) : NumbersUiState()
     data class ListError(val message: String?) : NumbersUiState()
 
